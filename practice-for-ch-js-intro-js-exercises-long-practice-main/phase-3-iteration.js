a4f8126f33fa144cb93fa4acdda9b622a82164e5
Dylan <dylanquach@gmail.com>
2023-10-31 15:55:44 -0700

    Create recursions

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,29 @@
 // Your code here
+Array.prototype.bubbleSort = function () {
+    let sorted = false
+    while (!sorted) {
+        sorted = true
+        for (let i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i+1]) {
+                [this[i], this[i+1]] = [this[i + 1], this[i]]
+                sorted = false
+            }
+        }
+    }
+    return this
+}
+
+// console.log([1, 5, 3, 4, 6, 2].bubbleSort())
+
+String.prototype.substrings = function() {
+    let arr = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i+1; j < this.length + 1; j++) {
+            arr.push(this.slice(i, j))
+        }
+    }
+    return arr
+}
+
+// console.log("asdf".substrings())
+// [a, as, asd, asdf, s, sd, sdf, d, df, f]
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
@@ -1,1 +1,57 @@
 // Your code here
+// const range = function(start, end) {
+//     if (start === end) {
+//         return [start]
+//     }
+//     let arr = range(start + 1, end)  // range(7, 10) -> [7,8,9,10],
+//     arr.unshift(start)
+//     return arr
+// }
+
+
+// function range(start, end) {
+//     if (start === end) {
+//         return [start]
+//     }
+//     let arr = range(start + 1, end)  // range(7, 10) -> [7,8,9,10],
+//     arr.unshift(start)
+//     return arr
+// }
+
+const range = (start, end) => {
+    if (start === end) {
+        return [start]
+    }
+    let arr = range(start + 1, end)  // range(7, 10) -> [7,8,9,10],
+    arr.unshift(start)
+    return arr
+}
+
+// console.log(range(1, 3)) // [1,2,3,4,5,6,7,8,9,10]
+
+const sumRec = function(arr) {
+    if (arr.length === 1) {
+        return arr[0]
+    }
+    const lastVal = arr.pop()
+    let sum = sumRec(arr)
+    return lastVal + sum
+}
+
+// console.log(sumRec([1,2,3, 10])) // 6
+
+const exponent1 = function(base, exp) {
+    if (exp === 0) return 1
+    let val = exponent1(base, exp - 1)
+    return val * base
+}
+
+const exponent2 = function(base, exp) {
+    if (exp === 0) return 1
+    if (exp === 1) return base
+    if (exp % 2 === 0) {
+        let val = exponent1(base, exp / 2)^2
+    }
+}
+
+console.log(exponent2(3, 3)) // 3 * 3 * 3 * 1
